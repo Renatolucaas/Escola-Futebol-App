b1b17d638793ba38c5ee0d916a7921f222225b33
NANI <nanimcosta@ucl.br>
2026-04-26 15:04:21 -0300

    bibliotecas ultilizadas

---
+++ app/src/androidTest/java/com/example/escoladefutebolapp/activity/ForgotPasswordActivity.kt
@@ -0,0 +1,17 @@
+//package com.example.escolafutebolapp.ui.activity
+//
+//import UserRepository
+//import android.graphics.Typeface
+//import android.os.Bundle
+//import android.text.InputType
+//import android.view.Gravity
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.*
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.isVisible
+//import androidx.lifecycle.ViewModelProvider
+//import com.example.escolafutebolapp.R
+//import com.example.escolafutebolapp.viewmodel.ForgotPasswordUiState
+//import com.example.escolafutebolapp.viewmodel.ForgotPasswordViewModel
+//import com.google.firebase.firestore.FirebaseFirestore
