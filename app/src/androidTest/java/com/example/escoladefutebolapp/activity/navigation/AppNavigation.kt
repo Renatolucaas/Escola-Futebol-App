42b30fed86f074e8cdb13525da8da2bcb4a797d9
NANI <nanimcosta@ucl.br>
2026-04-26 16:12:00 -0300

    definindo a navegação com Jetpack Compose

--- app/src/androidTest/java/com/example/escoladefutebolapp/activity/navigation/AppNavigation.kt
+++ app/src/androidTest/java/com/example/escoladefutebolapp/activity/navigation/AppNavigation.kt
@@ -12,3 +12,31 @@ import com.example.escolafutebolapp.ui.screens.ForgotPasswordScreen
 import com.example.escolafutebolapp.ui.screens.LoginScreen
 import com.example.escolafutebolapp.ui.screens.RegisterScreen
 import com.example.escolafutebolapp.ui.screens.TreinosScreen
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("welcome/{userName}") { backStackEntry ->
+            val userName = backStackEntry.arguments?.getString("userName") ?: "Jogador"
+            WelcomeScreen(navController, userName)
+        }
+        composable("login") { LoginScreen(navController) }
+        composable("forgot_password") { ForgotPasswordScreen(navController) }
+        composable("register") { RegisterScreen(navController) }
+
+        // ✅ CERTIFIQUE-SE DE QUE ESTA ROTA EXISTE COM DOIS PARÂMETROS
+        composable("agenda/{userId}/{userTipo}") { backStackEntry ->
+            val userId = backStackEntry.arguments?.getString("userId") ?: ""
+            val userTipo = backStackEntry.arguments?.getString("userTipo") ?: "aluno"
+            AgendaScreen(navController, userId, userTipo)
+        }
+
+        composable("treino") { TreinosScreen(navController) }
+    }
+}
