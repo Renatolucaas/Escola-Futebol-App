74ae171b8441dfe25d4549095c64ccf646ccb0f0
NANI <nanimcosta@ucl.br>
2026-04-26 16:08:32 -0300

    bibliotecas ultilizadas

---
+++ app/src/androidTest/java/com/example/escoladefutebolapp/activity/navigation/AppNavigation.kt
@@ -0,0 +1,14 @@
+package com.example.escolafutebolapp.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.escolafutebolapp.ui.screens.WelcomeScreen
+import com.example.escolafutebolapp.ui.screens.AgendaScreen
+import com.example.escolafutebolapp.ui.screens.ForgotPasswordScreen
+import com.example.escolafutebolapp.ui.screens.LoginScreen
+import com.example.escolafutebolapp.ui.screens.RegisterScreen
+import com.example.escolafutebolapp.ui.screens.TreinosScreen
