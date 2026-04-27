b4b41a86a6a81ab94e1da3c46c19e40cbeab4a2a
NANI <nanimcosta@ucl.br>
2026-04-27 13:55:59 -0300

    bibioltecas ultilizadas

---
+++ app/src/androidTest/java/service/RealTimeDBService.kt
@@ -0,0 +1,17 @@
+package com.example.escolafutebolapp.service
+
+import com.example.escolafutebolapp.models.User
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
