6a387e05d67e1ca981ae5be7068137cf6be04a2f
uiraseb <uiraseeberger@ucl.br>
2026-04-27 19:42:33 -0300

    bibliotecas ultilizadas do Lifecycle

---
+++ app/src/androidTest/java/uitls/AppLifecycleObserver.kt
@@ -0,0 +1,10 @@
+package com.example.escolafutebolapp.uitls
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.example.escolafutebolapp.service.RealtimeDBService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
