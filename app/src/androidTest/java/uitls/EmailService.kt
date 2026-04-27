ac0fea8dce524eab34f2a7e7d0cc7c6eb695dbbb
uiraseb <uiraseeberger@ucl.br>
2026-04-27 20:14:20 -0300

    Bibliotecas ultilizadas

---
+++ app/src/androidTest/java/uitls/EmailService.kt
@@ -0,0 +1,9 @@
+package com.example.escolafutebolapp.service
+
+import android.content.Context
+import java.util.Calendar
+import java.util.Properties
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
